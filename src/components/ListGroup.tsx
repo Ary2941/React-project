7cb26a95aeeb7f0755cdc50e260721619788c59a
Ary2941 <amaury2941@gmail.com>
2024-07-18 21:15:13 -0300

    componet listGroup agora é interativo :)

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,12 +1,22 @@
 function ListGroup() {
+  let items = ["item 1", "item 2", "item 3", "item 4"];
+
+  items = [];
+
+  const getMessage = () => {
+    return items.length === 0 && <p> Nothing found</p>;
+  }
+  
   return (
-    <ul className="list-group">
-      <li className="list-group-item">An item</li>
-      <li className="list-group-item">A second item</li>
-      <li className="list-group-item">A third item</li>
-      <li className="list-group-item">A fourth item</li>
-      <li className="list-group-item">And a fifth one</li>
-    </ul>
+    <>
+      <h1> ListGroup </h1>
+      {getMessage}
+      <ul className="list-group">
+        {items.map((item) => (
+          <li key={item}>{item}</li>
+        ))}
+      </ul>
+    </>
   );
 }
 
