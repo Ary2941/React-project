349ce195870c877322e0cf2ad674b2bd8016c21a
Ary2941 <amaury2941@gmail.com>
2024-07-20 07:22:45 -0300

    agora tem estados :o

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,19 +1,28 @@
+import { MouseEvent, useState } from "react";
+
 function ListGroup() {
   let items = ["item 1", "item 2", "item 3", "item 4"];
-
-  items = [];
+  // StateHook
+  const [selectedIndex, setSelectedIndex] = useState (-1);
+   // arr[0]variável (selectedIndex)
+  //  arr[1]função de att.
 
   const getMessage = () => {
     return items.length === 0 && <p> Nothing found</p>;
   }
+
+  // event handle
+  const handleClickShowEvent = (event: MouseEvent) => {
+    console.log(event)
+  };
   
   return (
     <>
-      <h1> ListGroup </h1>
+      <h1 className="title"> ListGroup </h1>
       {getMessage}
       <ul className="list-group">
-        {items.map((item) => (
-          <li key={item}>{item}</li>
+        {items.map((item, index) => (
+          <li className={selectedIndex === index? "list-group-item active":"list-group-item"} key={item} onClick={()=> {setSelectedIndex(index)}} >{item}</li>
         ))}
       </ul>
     </>
