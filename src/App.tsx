e4f868078ad912d381612db2cd3f89717f3eacd1
Ary2941 <amaury2941@gmail.com>
2024-07-23 22:06:09 -0300

    children

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
-import ListGroup from "./components/ListGroup"
+//import ListGroup from "./components/ListGroup"
+import Alert from "./components/alert"
+
 function App(){
-  let items = ["item 1", "item 2", "item 3", "item 4"];
+/*   let items = ["item 1", "item 2", "item 3", "item 4"];
   const handleSelectItem = (item:string) =>{
     console.log(item);
   }
 
   return <div><ListGroup items={items} heading="Cities" onSelectItem={handleSelectItem}/></div>
+ */
+  return <div><Alert><h1>Rocambole</h1></Alert></div>
 }
 
 export default App;
